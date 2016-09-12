3ac1fa7cdbcd43f49671a6873b08d95e3f1f8c34
Angel Vanegas <angelman@gmail.com>
2016-09-12 14:46:50 -0400

    --amend

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response){
   response.render('public/index');
-};
+});
 
 app.listen(app.get('port'), function(){
   console.log("Node app running on port", app.get('port'));
